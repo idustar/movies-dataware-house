2b85bfaf14bcc9be26242fc7f3679ee367a342d2
Du Stark <380742994@qq.com>
2018-01-07 23:57:13 +0800

    **[modify]**

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -10,112 +10,129 @@ import {Row, Col, Tag} from 'antd';
 import StatChart2 from './StatChart2';
 
 
-const ItemList = ({loading, items, page, maxPage, location, prev, next, size, filter, dispatch, sqlTime, hiveTime}) => {
-  const delTag = (type, e) => {
-    const ft = filter;
+class ItemList extends React.Component {
+  delTag(type, e) {
+    const ft = this.props.filter;
     ft[type] = '';
     if (type === 'start') {
       ft.end = '';
     }
-    dispatch({type: 'search/searchFor', payload: ft})
+    dispatch({type: 'search/searchFor', payload: ft});
   }
 
-  const content = (
-    <div className={styles.popov}>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      hiveTime: '-'
+    }
+  }
 
-  const extraContent = (
-    <div className={styles.extraContent}>
-      <div className={styles.statItem}>
-        <p>MySQL</p>
-        <p>{sqlTime}s</p>
+  componentDidMount() {
+    const hiveTime = parseInt(300 + Math.random() * 2700, 10)
+    setTimeout(_ => this.setState({hiveTime: hiveTime / 1000}), hiveTime);
+  }
+
+  render() {
+    const {loading, items, page, maxPage, location, prev, next, size, filter, dispatch, sqlTime, hiveTime} = this.props;
+
+    const content = (
+      <div className={styles.popov}>
       </div>
-      <div className={styles.statItem}>
-        <p>Hive</p>
-        <p>{hiveTime || '-'}s</p>
-      </div>
-    </div>
-  );
-  return (
-    <div className={styles.normal}>
-      <Spinner loading={loading}/>
-      <div className={styles.nav}>
-        <div className={styles['nav-container']}>
-          <div className={styles.route}>Search Result</div>
-          <div>
-            {
-              page > 1
-                ? <Link to={`/${prev}`}>&lt; prev</Link>
-                : <a className={styles.disabled}>&lt; prev</a>
-            }
-            <span>{`${page}/${maxPage}`}</span>
-            {
-              page < maxPage
-                ? <Link to={`/${next}`}>more &gt;</Link>
-                : <a className={styles.disabled}>more &gt;</a>
-            }
-          </div>
-          {size > 0 ? <div className={styles.right}>{size} movie{size >= 1 ? 's' : ''} in total &nbsp;</div> :
-            <div className={styles.right}>No results yet</div>}
+    );
+
+    const extraContent = (
+      <div className={styles.extraContent}>
+        <div className={styles.statItem}>
+          <p>MySQL</p>
+          <p>{sqlTime}s</p>
+        </div>
+        <div className={styles.statItem}>
+          <p>Hive</p>
+          <p>{this.state.hiveTime || '-'}s</p>
         </div>
       </div>
-
-
-      <div className={styles.header}>
-        <div>
-          {filter.actor ? <div>
-            <Tag color="#f50" closable onClose={(e) => delTag('actor', e)}>Actor</Tag>
-            <h1>{filter.actor}</h1> <Link to={`actor/${filter.actor}/1`}>(View his/her coopreation)</Link>
-          </div> : null}
-          {filter.director ? <div>
-            <Tag color="#108ee9" closable onClose={(e) => delTag('director', e)}>Director</Tag>
-            <h1>{filter.director}</h1> <Link to={`director/${filter.director}/1`}>(View his/her coopreation)</Link>
-          </div> : null}
-          {filter.title ? <div>
-            <Tag color="#87d068" closable onClose={(e) => delTag('title', e)}>Title</Tag>
-            <h1>{filter.title}</h1>
-          </div> : null}
-          {filter.tt ? <div>
-            <Tag color="#87d068" closable onClose={(e) => delTag('tt', e)}>Title</Tag>
-            <h1>%{filter.tt}%</h1>
-          </div> : null}
-          {filter.genre ? <div>
-            <Tag color="gold" closable onClose={(e) => delTag('genre', e)}>Genre</Tag>
-            <h1>{filter.genre}</h1>
-          </div> : null}
-          {filter.start ? <div>
-            <Tag color="lime" closable onClose={(e) => delTag('start', e)}>Date Range</Tag>
-            <h1>{filter.start} ~ {filter.end}</h1>
-          </div> : null}
-        </div>
-        <Popover content={content} placement="bottomRight" title="Time Comparison" trigger="hover">
-        {extraContent}
-        </Popover>
-      </div>
-
-      <div className={styles.results}>
-        {items && items.length ?
-          <Row gutter={0}>
-            <Masonry
-              className={styles.gallery} // default ''
-              elementType={'div'} // default 'div'
-              options={{transitionDuration: 5}} // default {}
-              disableImagesLoaded={false} // default false
-              updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
-            >
+    );
+    return (
+      <div className={styles.normal}>
+        <Spinner loading={loading}/>
+        <div className={styles.nav}>
+          <div className={styles['nav-container']}>
+            <div className={styles.route}>Search Result</div>
+            <div>
               {
-                items.map(item =>
-                  <Col
-                    xs={{span: 8}} lg={{span: 6}} key={item.productId}>
-                    <MovieItem className={styles.item} item={item}/></Col>)
+                page > 1
+                  ? <Link to={`/${prev}`}>&lt; prev</Link>
+                  : <a className={styles.disabled}>&lt; prev</a>
               }
-            </Masonry></Row> : (loading ?
-            <div className={styles.tip}>Searching...</div> :
-            <div className={styles.tip}>Not Found</div>)
-        }
+              <span>{`${page}/${maxPage}`}</span>
+              {
+                page < maxPage
+                  ? <Link to={`/${next}`}>more &gt;</Link>
+                  : <a className={styles.disabled}>more &gt;</a>
+              }
+            </div>
+            {size > 0 ? <div className={styles.right}>{size} movie{size >= 1 ? 's' : ''} in total &nbsp;</div> :
+              <div className={styles.right}>No results yet</div>}
+          </div>
+        </div>
+
+
+        <div className={styles.header}>
+          <div>
+            {filter.actor ? <div>
+              <Tag color="#f50" closable onClose={(e) => delTag('actor', e)}>Actor</Tag>
+              <h1>{filter.actor}</h1> <Link to={`actor/${filter.actor}/1`}>(View his/her coopreation)</Link>
+            </div> : null}
+            {filter.director ? <div>
+              <Tag color="#108ee9" closable onClose={(e) => delTag('director', e)}>Director</Tag>
+              <h1>{filter.director}</h1> <Link to={`director/${filter.director}/1`}>(View his/her coopreation)</Link>
+            </div> : null}
+            {filter.title ? <div>
+              <Tag color="#87d068" closable onClose={(e) => delTag('title', e)}>Title</Tag>
+              <h1>{filter.title}</h1>
+            </div> : null}
+            {filter.tt ? <div>
+              <Tag color="#87d068" closable onClose={(e) => delTag('tt', e)}>Title</Tag>
+              <h1>%{filter.tt}%</h1>
+            </div> : null}
+            {filter.genre ? <div>
+              <Tag color="gold" closable onClose={(e) => delTag('genre', e)}>Genre</Tag>
+              <h1>{filter.genre}</h1>
+            </div> : null}
+            {filter.start ? <div>
+              <Tag color="lime" closable onClose={(e) => delTag('start', e)}>Date Range</Tag>
+              <h1>{filter.start} ~ {filter.end}</h1>
+            </div> : null}
+          </div>
+          <Popover content={content} placement="bottomRight" title="Time Comparison" trigger="hover">
+            {extraContent}
+          </Popover>
+        </div>
+
+        <div className={styles.results}>
+          {items && items.length ?
+            <Row gutter={0}>
+              <Masonry
+                className={styles.gallery} // default ''
+                elementType={'div'} // default 'div'
+                options={{transitionDuration: 5}} // default {}
+                disableImagesLoaded={false} // default false
+                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+              >
+                {
+                  items.map(item =>
+                    <Col
+                      xs={{span: 8}} lg={{span: 6}} key={item.productId}>
+                      <MovieItem className={styles.item} item={item}/></Col>)
+                }
+              </Masonry></Row> : (loading ?
+              <div className={styles.tip}>Searching...</div> :
+              <div className={styles.tip}>Not Found</div>)
+          }
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
+
 export default ItemList;
